4dee96d6487f1e59ab9e14f0159c6ed9cfca0031
angrybunny123 <kennethteo98@gmail.com>
2020-06-24 18:19:29 +0800

    questions can now be from 5-10

--- src/components/Account/AccountTable/createdQuizzesTable.js
+++ src/components/Account/AccountTable/createdQuizzesTable.js
@@ -1,36 +1,39 @@
 import React from "react";
 
+import classes from "./accountTable.module.css";
 import { Table } from "react-bootstrap";
 
 const createdQuizzesTable = (props) => {
-  let table = <div>No quizzes created yet :( go create a quiz now!</div>;
-  if (props.quizzes !== undefined) {
+  let table = (
+    <div className={classes.noQuizzes}>
+      No quizzes created yet :( go create a quiz now!
+    </div>
+  );
+  if (props.quizzes.length !== 0) {
     table = (
-      <tbody>
-        {props.quizzes.map((quiz, index) => (
+      <Table responsive variant="dark">
+        <thead>
           <tr>
-            <td>{index + 1}</td>
-            <td>{quiz.category}</td>
-            <td>{quiz.name}</td>
-            <td>{quiz.popularity}</td>
+            <th>#</th>
+            <th>Category</th>
+            <th>Quiz Name</th>
+            <th>Attempts</th>
           </tr>
-        ))}
-      </tbody>
+        </thead>
+        <tbody>
+          {props.quizzes.map((quiz, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{quiz.category}</td>
+              <td>{quiz.name}</td>
+              <td>{quiz.popularity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     );
   }
-  return (
-    <Table responsive variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Category</th>
-          <th>Quiz Name</th>
-          <th>Attempts</th>
-        </tr>
-      </thead>
-      {table}
-    </Table>
-  );
+  return table;
 };
 
 export default createdQuizzesTable;
--- src/components/Account/AccountTable/quizHistoryTable.js
+++ src/components/Account/AccountTable/quizHistoryTable.js
@@ -1,38 +1,41 @@
 import React from "react";
 
 import { Table } from "react-bootstrap";
+import classes from "./accountTable.module.css";
 
 const quizHistoryTable = (props) => {
-  let table = <div>No quizzes attempted yet :( go attempt a quiz now!</div>;
+  let table = (
+    <div className={classes.noQuizzes}>
+      No quizzes attempted yet :( go attempt a quiz now!
+    </div>
+  );
   if (props.quizzes !== undefined) {
     table = (
-      <tbody>
-        {props.quizzes.map((quiz, index) => (
+      <Table responsive variant="dark">
+        <thead>
           <tr>
-            <td>{index + 1}</td>
-            <td>{quiz.category}</td>
-            <td>{quiz.name}</td>
-            <td>
-              {quiz.score}/{quiz.questionNumber}
-            </td>
+            <th>#</th>
+            <th>Category</th>
+            <th>Quiz Name</th>
+            <th>Score</th>
           </tr>
-        ))}
-      </tbody>
+        </thead>
+        <tbody>
+          {props.quizzes.map((quiz, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{quiz.category}</td>
+              <td>{quiz.name}</td>
+              <td>
+                {quiz.score}/{quiz.questionNumber}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     );
   }
-  return (
-    <Table responsive variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Category</th>
-          <th>Quiz Name</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      {table}
-    </Table>
-  );
+  return table;
 };
 
 export default quizHistoryTable;
