e15114b3d787ed59097abbba5ecb52e26bfbe52a
angrybunny123 <kennethteo98@gmail.com>
2020-07-23 02:53:23 +0800

    added button to view all sheeps in home

--- src/components/Homepage/SheepRankings/SheepRankings.js
+++ src/components/Homepage/SheepRankings/SheepRankings.js
@@ -4,14 +4,21 @@ import { Table } from "react-bootstrap";
 import classes from "./SheepRankings.module.css";
 
 class SheepRankings extends Component {
+  state = {
+    showAllSheeps: false,
+  };
   render() {
     let table = (
       <p className={classes.Unattempted}>NO SHEEPS IN THIS APP? WTF</p>
     );
     if (this.props.array !== undefined && this.props.array !== "") {
-      const sortedArray = this.props.array
-        .sort((a, b) => (a.sheepPoints > b.sheepPoints ? -1 : 1))
-        .slice(0, 3);
+      let sortedArray = this.props.array.sort((a, b) =>
+        a.sheepPoints > b.sheepPoints ? -1 : 1
+      );
+
+      if (this.state.showAllSheeps === false) {
+        sortedArray = sortedArray.slice(0, 3);
+      }
       table = (
         <Table responsive>
           <thead>
@@ -26,7 +33,7 @@ class SheepRankings extends Component {
               <tr>
                 <td>{index + 1}</td>
                 <td>@{item.username}</td>
-                <td>{item.sheepPoints}</td>
+                <td>{Math.round(item.sheepPoints)}</td>
               </tr>
             ))}
           </tbody>
@@ -37,6 +44,20 @@ class SheepRankings extends Component {
       <div>
         <p className={classes.Title}>All Time Scholar Sheeps</p>
         {table}
+        <button
+          className={classes.buttons}
+          onClick={() => {
+            this.setState((prevState) => {
+              return {
+                showAllSheeps: !prevState.showAllSheeps,
+              };
+            });
+          }}
+        >
+          {this.state.showAllSheeps
+            ? ">>View less sheeps"
+            : ">>View all sheeps"}
+        </button>
       </div>
     );
   }
