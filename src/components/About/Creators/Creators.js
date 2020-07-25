f8d98f5b8ad007ab5043d64d48418910fc39cacb
Penn Han <leepennhan98@gmail.com>
2020-07-26 00:13:50 +0800

    About page

--- src/components/About/Creators/Creators.js
+++ src/components/About/Creators/Creators.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-// import classes from "./Creators.module.css";
+import classes from "./Creators.module.css";
 
 const creators = (props) => {
   return (
     <div>
-      <Card style={{ width: "20rem", margin: "auto" }}>
+      <Card style={{ width: "25rem", margin: "auto" }}>
         <Card.Header>{props.creator}</Card.Header>
         <Card.Img varient="top" src={props.image} />
         <Card.Body>
           <Card.Text>{props.creatordesc}</Card.Text>
           <a href={props.link}>
-            <button>Connect Now!</button>
+            <button className={classes.buttonStyle}>Connect Now!</button>
           </a>
         </Card.Body>
       </Card>
